43b2fe01b87dcc28db1bb1fd019815a1ebc910aa
Casey Ross <caseyross003@gmail.com>
2020-03-27 11:54:47 -0500

    add random list of users and items

--- transaction_generator.py
+++ transaction_generator.py
@@ -3,6 +3,28 @@ import random
 import string
 from datetime import datetime
 
+USERS = [
+    "casey@ocistream.com",
+    "thomas@ocistream.com",
+    "riaz@ocistream.com",
+    "madhu@ocistream.com",
+    "will@ocistream.com",
+    "debbie@ocistream.com"
+]
+
+ITEMS = [
+    ("Men's Socks", 9.99),
+    ("Women's Socks", 12.99),
+    ("Men's Shorts", 34.99),
+    ("Women's Shorts", 29.99),
+    ("Men's Jeans", 49.99),
+    ("Women's Jeans", 49.99),
+    ("Men's Dress Shirt", 54.99),
+    ("Women's Blouse", 54.99),
+    ("Men's Shoes", 89.99),
+    ("Women's Shoes", 99.99)
+]
+
 # Used for testing
 # Will update the functions to return random values
 def generate_key():
@@ -10,10 +32,10 @@ def generate_key():
     return ''.join(random.choice(options) for i in range(8))
 
 def generate_user():
-    return "casey@ocistream.com"
+    return random.choice(USERS)
 
 def generate_item():
-    return ("Men's Shorts", 29.99)
+    return random.choice(ITEMS)
 
 def generate_date():
     return str(datetime.now())
